b42c71d2a435b7e86a7c72f0bd04ba27bae0c1cb
vadim <gabderahmanov99@gmail.com>
2024-02-10 21:22:39 +0300

    add textview

--- app/src/main/java/com/abg/wordfinder/MainActivity.java
+++ app/src/main/java/com/abg/wordfinder/MainActivity.java
@@ -1,16 +1,23 @@
 package com.abg.wordfinder;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.abg.wordfinder.model.Word;
 import com.abg.wordfinder.view.WordSearchView;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        String[] words = {"hello", "world", "java", "android", "cell", "fog", "long"};
+        String[] words = {"василий", "андроид", "java", "анонимус", "cell", "fog", "long"};
+        Map<String, TextView> map = new HashMap<>();
 
         WordSearchGenerator wordSearchGenerator = new WordSearchGenerator(10, 10, words);
 
@@ -32,13 +40,22 @@ public class MainActivity extends AppCompatActivity {
             Log.d("cc", c.toString());
         }
 
-        TextView tvStrikethrough = findViewById(R.id.tv_strikethrough);
-        tvStrikethrough.setText("Word\nSome\nSearching\nFog");
-        tvStrikethrough.setPaintFlags(tvStrikethrough.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        LinearLayout linearForText = (LinearLayout) findViewById(R.id.linearForText);
+
+        for (String s : words) {
+            TextView textView = new TextView(this);
+            textView.setTextSize(25);
+            textView.setTextColor(Color.WHITE);
+            textView.setText(s);
+            linearForText.addView(textView);
+            map.put(s, textView);
+        }
 
         WordSearchView wordsGrid = findViewById(R.id.wordsGrid);
         wordsGrid.setLetters(wordSearchGenerator.getBoard());
         wordsGrid.setWords(wordSearchGenerator.getWords());
-        wordsGrid.setOnWordSearchedListener(word -> Toast.makeText(MainActivity.this, word + " found", Toast.LENGTH_SHORT).show());
+        wordsGrid.setOnWordSearchedListener(word -> {
+            Objects.requireNonNull(map.get(word)).setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
+        });
     }
 }
