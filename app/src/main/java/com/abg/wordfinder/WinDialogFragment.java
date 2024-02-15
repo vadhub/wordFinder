69e7b7d48fa830136482e16eb8dae45ec7173596
vadim <gabderahmanov99@gmail.com>
2024-02-15 19:02:18 +0300

    time show in win

--- app/src/main/java/com/abg/wordfinder/WinDialogFragment.java
+++ app/src/main/java/com/abg/wordfinder/WinDialogFragment.java
@@ -1,10 +1,12 @@
 package com.abg.wordfinder;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,16 +20,25 @@ public class WinDialogFragment extends DialogFragment {
     }
 
     private Listener listener;
+    private String time;
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 
     public void setListener(Listener listener) {
         this.listener = listener;
     }
 
+    @SuppressLint("SetTextI18n")
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
         View view = getLayoutInflater().inflate(R.layout.fragment_congratilation, null);
+
+        TextView textView = view.findViewById(R.id.time_win);
+        textView.setText(getString(R.string.your_time) + time);
         Button b = view.findViewById(R.id.restart);
         b.setOnClickListener(v -> {
             listener.restart();
